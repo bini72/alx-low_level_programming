157b1e3ce370946927df2218e8e3471c59d98de3
biniam hadush <biniamemebet234@gmail.com>
2022-03-10 09:13:24 -0800

    size of varios types

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -17,6 +17,6 @@ printf("Size of a char: %zu byte(s)\n", sizeof(acharacter));
 printf("Size of an int: %zu byte(s)\n", sizeof(ainteger));
 printf("Size of a long int: %zu byte(s)\n", sizeof(along));
 printf("Size of a long long int: %zu byte(s)\n", sizeof(alonglong));
-prints("Size of a float: %zu byte(s)\n", sizeof(afloat));
+printf("Size of a float: %zu byte(s)\n", sizeof(afloat));
 return (0);
 }
